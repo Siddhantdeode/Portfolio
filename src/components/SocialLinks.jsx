bfcd5af32779dc9365a0146334df29a76fc9327f
SIDDHANT RAJPUT <sidxdeoda07@gmail.com>
2024-03-20 11:36:06 +0530

    Portfolio Done

---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import { FaLinkedin , FaGithubSquare } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+
+const SocialLinks = () => {
+
+    const links = [
+        {
+          id: 1,
+          child: (
+            <>
+              LinkedIn <FaLinkedin size={30} />
+            </>
+          ),
+          href: "https://www.linkedin.com/in/siddhant-rajput/",
+          style: "rounded-tr-md",
+        },
+        {
+          id: 2,
+          child: (
+            <>
+              GitHub <FaGithubSquare  size={30} />
+            </>
+          ),
+          href: "https://github.com/Siddhantdeode",
+        },
+        {
+          id: 3,
+          child: (
+            <>
+              Gmail <SiGmail size={30} />
+            </>
+          ),
+          href: "mailto:siddhantdeode07@gmail.com",
+        },
+        {
+          id: 4,
+          child: (
+            <>
+              Resume <BsFillPersonLinesFill size={30} />
+            </>
+          ),
+          href: "/SIDDHANT DEODE  DA RESUME.pdf",
+          style: "rounded-br-md",
+          download: true,
+        },
+      ]
+    
+
+    return (
+        <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
+          <ul>
+            {links.map(({ id, child, href, style, download }) => (
+              <li
+                key={id}
+                className={
+                  "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
+                  " " +
+                  style
+                }
+              >
+                <a
+                  href={href}
+                  className="flex justify-between items-center w-full text-white"
+                  download={download}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {child}
+                </a>
+              </li>
+            ))}
+     </ul>
+    </div>
+  )
+}
+
+export default SocialLinks
