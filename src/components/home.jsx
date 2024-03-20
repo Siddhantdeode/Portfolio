34da48a574b8b087f430131ef27d6a3c91f01585
SIDDHANT RAJPUT <sidxdeoda07@gmail.com>
2024-03-20 11:53:44 +0530

    minor chng

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -15,7 +15,8 @@ const Home = () => {
         <div className='flex flex-col justify-center h-full'> 
           <h2 className='text-4xl sm:text-7xl font-bold text-white'> I'm a Full Stack Developer.</h2>
           <p className='text-gray-500 py-4 max-w-md'>
-            My introduction...Neque porro quisquam est
+            My introduction...
+            Hi I'm Siddhant Rajput I come From Nagpur Maharasthra 
              qui dolorem ipsum quia dolor sit amet, 
             consectetur, adipisci velit..."
            "There is no one who loves pain itself,
